cc0a3946db6de11967c797610a3712a0ba64eff2
Daniil Prokhorov <daniilprokh@gmail.com>
2023-08-26 20:57:43 +0000

    Редактирование комманд

--- lab_01/database/user.cpp
+++ lab_01/database/user.cpp
@@ -29,7 +29,7 @@ User User::FromJSON(const std::string& str) {
     Var result = parser.parse(str);
     Object::Ptr object = result.extract<Object::Ptr>();
 
-    user.SetId(object->getValue<uint64_t>("id"));
+    user.SetId(object->getValue<uint64_t>("user_id"));
     user.SetFirstName(object->getValue<std::string>("first_name"));
     user.SetLastName(object->getValue<std::string>("last_name"));
     user.SetEmail(object->getValue<std::string>("email"));
@@ -42,7 +42,7 @@ User User::FromJSON(const std::string& str) {
 Object::Ptr User::ToJSON() const {
     Object::Ptr root = new Object();
 
-    root->set("id", id_);
+    root->set("user_id", id_);
     root->set("first_name", first_name_);
     root->set("last_name", last_name_);
     root->set("email", email_);
@@ -58,13 +58,17 @@ void User::Init() {
         Database& database = Database::GetInstance();
         Session session = database.CreateSession();
         Statement create_stmt(session);
-        create_stmt << "CREATE TABLE IF NOT EXISTS `User` (`id` INT NOT NULL AUTO_INCREMENT,"
-                    << "`first_name` VARCHAR(256) NOT NULL,"
-                    << "`last_name` VARCHAR(256) NOT NULL,"
-                    << "`login` VARCHAR(256) NOT NULL,"
-                    << "`password` VARCHAR(256) NOT NULL,"
-                    << "`email` VARCHAR(256) NULL,"
-                    << "PRIMARY KEY (`id`),KEY `fn` (`first_name`),KEY `ln` (`last_name`));",
+        create_stmt << "CREATE TABLE IF NOT EXISTS users ("
+                    << "PRIMARY KEY (user_id),"
+                    << "user_id INT NOT NULL AUTO_INCREMENT,"
+                    << "first_name  VARCHAR(256)    NOT NULL,"
+                    << "last_name   VARCHAR(256)    NOT NULL,"
+                    << "email       VARCHAR(256)    NOT NULL,"
+                    << "login       VARCHAR(256)    NOT NULL,"
+                    << "password    VARCHAR(256)    NOT NULL,"
+                    << "    INDEX name_index    USING   BTREE (first_name, last_name),"
+                    << "    INDEX email_index   USING   HASH  (email)"
+                    << ") CHARACTER SET utf8 COLLATE utf8_unicode_ci;",
             now;
     }
     catch (ConnectionException &e)
@@ -89,12 +93,12 @@ std::vector<User> User::ReadAll() {
         std::vector<User> result;
         User user;
         select << "SELECT "
-                  "id,"
+                  "user_id,"
                   "first_name,"
                   "last_name,"
                   "email,"
                   "login,"
-                  "password FROM User",
+                  "password FROM users",
             into(user.id_),
             into(user.first_name_),
             into(user.last_name_),
@@ -136,7 +140,7 @@ std::optional<User> User::ReadById(uint64_t id) {
                   "last_name,"
                   "email,"
                   "login,"
-                  "password FROM users where id=?",
+                  "password FROM users WHERE user_id = ?",
             into(user.id_),
             into(user.first_name_),
             into(user.last_name_),
@@ -173,7 +177,7 @@ std::optional<uint64_t> User::Authorize(std::string login,
         Statement select(session);
         long id;
         select << "SELECT "
-                  "user_id FROM users where login=? and password=?",
+                  "user_id FROM users WHERE login = ? and password = ?",
             into(id),
             use(login),
             use(password),
@@ -215,7 +219,7 @@ std::vector<User> User::Search(std::string firstName,
                   "last_name,"
                   "email,"
                   "login,"
-                  "password FROM users where first_name LIKE ? and last_name LIKE ?",
+                  "password FROM users WHERE first_name LIKE ? AND last_name LIKE ?",
             into(user.id_),
             into(user.first_name_),
             into(user.last_name_),
