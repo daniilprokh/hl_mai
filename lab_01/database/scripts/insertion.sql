e4bfc909e1267719dbb82bd8b2b28052c6dd4d29
Daniil Prokhorov <daniilprokh@gmail.com>
2023-08-26 20:49:10 +0000

    К данным добавлены логины и пароли пользователей

--- lab_01/database/scripts/insertion.sql
+++ lab_01/database/scripts/insertion.sql
@@ -1,13 +1,13 @@
 USE messenger;
 
-REPLACE INTO users (first_name, last_name, email) 
-VALUES ('Ульяна',    'Маркова',     'quibrerassesi-1827@test.com'), 
-       ('Даниил',    'Минин',       'preicagoigitri-1131@test.com'),  
-       ('Софья',     'Смирнова',    'xaxigrecrauleu-2263@test.com'), 
-       ('Александр', 'Соколов',     'tudeibrigoubreu-4529@test.com'),
-       ('Айлин',     'Павлова',     'framessukeyou-5980@test.com'),
-       ('Тимофей',   'Белоусов',    'troiprolizedda-5119@test.com'),
-       ('Ульяна',    'Жукова',      'deigegrifforeu-5947@test.com'),
-       ('Кирилл',    'Иванов',      'meiwaureropro-2522@test.com'),
-       ('Алексей',   'Леонов',      'creyeffenafra-1956@test.com'),
-       ('Денис',     'Калашников',  'peinnellewocau-4883@test.com')
+REPLACE INTO users (first_name, last_name, email, login, password) 
+VALUES ('Ульяна',    'Маркова',     'quibrerassesi-1827@test.com',    'Enally',     'cmeMvc'), 
+       ('Даниил',    'Минин',       'preicagoigitri-1131@test.com',   'Kyusai',     'IQzmTp'),  
+       ('Софья',     'Смирнова',    'xaxigrecrauleu-2263@test.com',   'Teo',        'FRLfgV'), 
+       ('Александр', 'Соколов',     'tudeibrigoubreu-4529@test.com',  'Xalw',       'Ktdeaf'),
+       ('Айлин',     'Павлова',     'framessukeyou-5980@test.com',    'Xira',       'j23LgK'),
+       ('Тимофей',   'Белоусов',    'troiprolizedda-5119@test.com',   'Jeueropry',  'hxNrd0'),
+       ('Ульяна',    'Жукова',      'deigegrifforeu-5947@test.com',   'Prdam',      '4V1wLy'),
+       ('Кирилл',    'Иванов',      'meiwaureropro-2522@test.com',    'Vaharoni',   'ylTAka'),
+       ('Алексей',   'Леонов',      'creyeffenafra-1956@test.com',    'Hangashoy',  '4zPxwl'),
+       ('Денис',     'Калашников',  'peinnellewocau-4883@test.com',   'Oleld',      'IF4Yok')
