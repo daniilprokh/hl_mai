84445271d6d0b23bc99462d9edbf55d91f639c8d
Daniil Prokhorov <daniilprokh@gmail.com>
2023-08-26 20:29:22 +0000

    Команды для заполнения базы данных

---
+++ lab_01/database/scripts/insertion.sql
@@ -0,0 +1,13 @@
+USE messenger;
+
+REPLACE INTO users (first_name, last_name, email) 
+VALUES ('Ульяна',    'Маркова',     'quibrerassesi-1827@test.com'), 
+       ('Даниил',    'Минин',       'preicagoigitri-1131@test.com'),  
+       ('Софья',     'Смирнова',    'xaxigrecrauleu-2263@test.com'), 
+       ('Александр', 'Соколов',     'tudeibrigoubreu-4529@test.com'),
+       ('Айлин',     'Павлова',     'framessukeyou-5980@test.com'),
+       ('Тимофей',   'Белоусов',    'troiprolizedda-5119@test.com'),
+       ('Ульяна',    'Жукова',      'deigegrifforeu-5947@test.com'),
+       ('Кирилл',    'Иванов',      'meiwaureropro-2522@test.com'),
+       ('Алексей',   'Леонов',      'creyeffenafra-1956@test.com'),
+       ('Денис',     'Калашников',  'peinnellewocau-4883@test.com')
